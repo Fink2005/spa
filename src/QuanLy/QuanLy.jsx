3f447301070cf1d99a6fd99dbabec91baa5d4c3a
Fink2005 <phans3806@gmail.com>
2025-02-02 15:45:08 +0700

    update manage

--- src/QuanLy/QuanLy.jsx
+++ src/QuanLy/QuanLy.jsx
@@ -31,87 +31,64 @@ export default function QuanLy() {
   };
 
   return (
-    <div className="p-5 sm:p-0 flex justify-center">
-      <Form
-        className="w-[1000px]"
-        name="basic"
-        style={{
-          maxWidth: 1000,
-        }}
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <div className="sm:flex justify-between">
-          <Form.Item label="Số hóa đơn" name="so_hd">
-            <Input className="lg:w-[300px]" placeholder="Số hóa đơn" />
-          </Form.Item>
-          <Form.Item
-            label="Khách hàng"
-            name="khach_hang"
-            rules={[
-              {
-                required: true,
-                message: "xin vui lòng điền tên khách hàng",
-              },
-            ]}
-          >
-            <Input className="lg:w-[300px]" placeholder="Tên khách hàng" />
-          </Form.Item>
-        </div>
-        <div className="sm:flex justify-between">
-          <Form.Item label="Dịch vụ dưỡng sinh" name="dv_duong_sinh">
-            <Input className="lg:w-[300px]" placeholder="Dịch vụ dưỡng sinh" />
-          </Form.Item>
-          <Form.Item label="Thẻ dịch vụ" name="the_dv">
-            <Input className="lg:w-[300px]" placeholder="Thẻ dịch vụ" />
-          </Form.Item>
-        </div>
-        <div className="sm:flex justify-between">
-          <Form.Item label="Dịch vụ spa" name="dv_spa">
-            <Input className="lg:w-[300px]" placeholder="Dịch vụ spa" />
-          </Form.Item>
-          <Form.Item label="Dịch vụ nail" name="dv_nail">
-            <Input className="lg:w-[300px]" placeholder="Dịch vụ nail" />
-          </Form.Item>
-        </div>
-        <div className="flex w-80 ">
-          <Form.Item
-            valuePropName="cash"
-            label={null}
-            rules={[
-              {
-                required: true,
-                message: "xin vui lòng điền tiền mặt",
-              },
-            ]}
-          >
-            <Checkbox onChange={handleCash}>Tiền mặt</Checkbox>
-          </Form.Item>
-          <Form.Item
-            valuePropName="banking"
-            label={null}
-            rules={[
-              {
-                required: true,
-                message: "xin vui lòng điền tiền chuyển",
-              },
-            ]}
-          >
-            <Checkbox onChange={handleBanking}>Chuyển khoản</Checkbox>
-          </Form.Item>
-        </div>
-        <div className="sm:flex justify-between">
-          <Form.Item label="Thẻ DV" name="the_dv_t">
-            <Input className="lg:w-[300px]" placeholder="thẻ DV" />
-          </Form.Item>
-          {cash && (
+    <div className="">
+      <h1 className="text-center text-3xl py-5 text-green-500">
+        Quản lí khách hàng Vi Tiên Cát
+      </h1>
+      <div className="p-5 sm:p-0 flex justify-center ms-5">
+        <Form
+          className="w-[1000px]"
+          name="basic"
+          style={{
+            maxWidth: 1000,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <div className="sm:flex justify-between">
+            <Form.Item label="Số hóa đơn" name="so_hd">
+              <Input className="lg:w-[300px]" placeholder="Số hóa đơn" />
+            </Form.Item>
             <Form.Item
-              label="Thanh toán bằng tiền mặt"
-              name="tien_mat"
+              label="Khách hàng"
+              name="khach_hang"
+              rules={[
+                {
+                  required: true,
+                  message: "xin vui lòng điền tên khách hàng",
+                },
+              ]}
+            >
+              <Input className="lg:w-[300px]" placeholder="Tên khách hàng" />
+            </Form.Item>
+          </div>
+          <div className="sm:flex justify-between">
+            <Form.Item label="Dịch vụ dưỡng sinh" name="dv_duong_sinh">
+              <Input
+                className="lg:w-[300px]"
+                placeholder="Dịch vụ dưỡng sinh"
+              />
+            </Form.Item>
+            <Form.Item label="Thẻ dịch vụ" name="the_dv">
+              <Input className="lg:w-[300px]" placeholder="Thẻ dịch vụ" />
+            </Form.Item>
+          </div>
+          <div className="sm:flex justify-between">
+            <Form.Item label="Dịch vụ spa" name="dv_spa">
+              <Input className="lg:w-[300px]" placeholder="Dịch vụ spa" />
+            </Form.Item>
+            <Form.Item label="Dịch vụ nail" name="dv_nail">
+              <Input className="lg:w-[300px]" placeholder="Dịch vụ nail" />
+            </Form.Item>
+          </div>
+          <div className="flex w-80 ">
+            <Form.Item
+              valuePropName="cash"
+              label={null}
               rules={[
                 {
                   required: true,
@@ -119,65 +96,96 @@ export default function QuanLy() {
                 },
               ]}
             >
-              <Input
-                className="lg:w-[300px]"
-                type="number"
-                placeholder="Tiền mặt"
-              />
+              <Checkbox onChange={handleCash}>Tiền mặt</Checkbox>
             </Form.Item>
-          )}
-        </div>
-        <div className="sm:flex justify-between">
-          <Form.Item
-            label="Nhân viên"
-            name="nhan_vien"
-            rules={[
-              {
-                required: true,
-                message: "xin vui lòng điền tên Nhân viên",
-              },
-            ]}
-          >
-            <Input className="lg:w-[300px]" placeholder="Tên nhân viên" />
-          </Form.Item>
-          {banking && (
             <Form.Item
-              label="Thanh toán bằng ngân hàng"
-              name="chuyen_khoan"
+              valuePropName="banking"
+              label={null}
               rules={[
                 {
                   required: true,
-                  message: "xin vui lòng điền số tiền chuyển khoản",
+                  message: "xin vui lòng điền tiền chuyển",
                 },
               ]}
             >
-              <Input
-                className="lg:w-[300px]"
-                type="number"
-                placeholder="Tiền ngân hàng"
-              />
+              <Checkbox onChange={handleBanking}>Chuyển khoản</Checkbox>
             </Form.Item>
-          )}
-        </div>
+          </div>
+          <div className="sm:flex justify-between">
+            <Form.Item label="Thẻ DV" name="the_dv_t">
+              <Input className="lg:w-[300px]" placeholder="thẻ DV" />
+            </Form.Item>
+            {cash && (
+              <Form.Item
+                label="Thanh toán bằng tiền mặt"
+                name="tien_mat"
+                rules={[
+                  {
+                    required: true,
+                    message: "xin vui lòng điền tiền mặt",
+                  },
+                ]}
+              >
+                <Input
+                  className="lg:w-[300px]"
+                  type="number"
+                  placeholder="Tiền mặt"
+                />
+              </Form.Item>
+            )}
+          </div>
+          <div className="sm:flex justify-between">
+            <Form.Item
+              label="Nhân viên"
+              name="nhan_vien"
+              rules={[
+                {
+                  required: true,
+                  message: "xin vui lòng điền tên Nhân viên",
+                },
+              ]}
+            >
+              <Input className="lg:w-[300px]" placeholder="Tên nhân viên" />
+            </Form.Item>
+            {banking && (
+              <Form.Item
+                label="Thanh toán bằng ngân hàng"
+                name="chuyen_khoan"
+                rules={[
+                  {
+                    required: true,
+                    message: "xin vui lòng điền số tiền chuyển khoản",
+                  },
+                ]}
+              >
+                <Input
+                  className="lg:w-[300px]"
+                  type="number"
+                  placeholder="Tiền ngân hàng"
+                />
+              </Form.Item>
+            )}
+          </div>
 
-        <div className=" ">
-          <Form.Item
-            label="Ghi chú"
-            name="ghi_chu"
-            className="w-full flex justify-start"
-          >
-            <textarea
-              placeholder="Ghi chú"
-              className="w-[400px] border-2 rounded-md"
-            ></textarea>
+          <div className=" ">
+            <Form.Item
+              label="Ghi chú"
+              name="ghi_chu"
+              className="w-full flex justify-start"
+            >
+              <textarea
+                placeholder="Ghi chú"
+                className="w-[400px] border-2 rounded-md"
+              ></textarea>
+            </Form.Item>
+          </div>
+          <Form.Item label={null}>
+            <Button htmlType="submit" className="bg-green-700">
+              Xác nhận
+            </Button>
           </Form.Item>
-        </div>
-        <Form.Item label={null}>
-          <Button type="primary" htmlType="submit">
-            Xác nhận
-          </Button>
-        </Form.Item>
-      </Form>
+        </Form>
+      </div>
     </div>
   );
 }
