c8486cd5e03ffc0520d1094f4f7839daf4b5cab1
Fink2005 <phans3806@gmail.com>
2025-05-02 14:37:42 +0700

    Update manage.webmanifest and remove favicon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 import Layout from "./template/Layout";
 import MainPage from "./MainPage/MainPage";
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Router,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import CompoSpa from "./ComboSpa/CompoSpa";
 import LamSachVaCapNuoc from "./LamSachVaCapNuoc/LamSachVaCapNuoc";
 import ChamSocDaSang from "./ChamSocDaSang/ChamSocDaSang";
@@ -9,10 +15,32 @@ import TreHoaNangCaoCam from "./TreHoaNangCaoCam/TreHoaNangCaoCam";
 import NanMunVaLamSach from "./NanMunVaLamSach/NanMunVaLamSach";
 import MenuSpa from "./MenuSpa/MenuSpa";
 import QuanLy from "./QuanLy/QuanLy";
+import { useEffect } from "react";
 
+function ManifestHandler() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Create or update manifest link
+    let manifestLink = document.querySelector('link[rel="manifest"]');
+    if (!manifestLink) {
+      manifestLink = document.createElement("link");
+      manifestLink.rel = "manifest";
+      document.head.appendChild(manifestLink);
+    }
+    // Set manifest based on path
+    manifestLink.href =
+      location.pathname === "/quan-ly"
+        ? "/manage.webmanifest"
+        : "/site.webmanifest";
+  }, [location.pathname]);
+
+  return null; // This component renders nothing
+}
 function App() {
   return (
     <BrowserRouter>
+      <ManifestHandler />
       <Routes>
         <Route path="/" element={<Layout content={<MainPage />} />} />
         <Route
